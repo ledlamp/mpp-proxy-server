5e76202387693b7d31075326531c8da377cbb954
Lamp <ledlamp89@gmail.com>
2020-11-03 13:28:15 -0800

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require("http-proxy").createServer({
-	target: 'ws://www.multiplayerpiano.com:443',
+	target: 'wss://www.multiplayerpiano.com',
 	ws: true
 }).listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
